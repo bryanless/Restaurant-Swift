0fd176c06e05f6544edb299f0501805245f98e8b
Bryan <71095749+bryanless@users.noreply.github.com>
2022-06-09 13:41:20 +0700

    Initial commit

---
+++ Restaurant/RestaurantApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantApp.swift
+//  Restaurant
+//
+//  Created by user on 09/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RestaurantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
